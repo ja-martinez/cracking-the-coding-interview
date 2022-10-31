3d83940a7597ba881563d8ecc9540192a762cd8c
clavius <16825879+ja-martinez@users.noreply.github.com>
2022-10-31 11:09:15 -0700

    finish 1.3

--- 1.3-urlify.js
+++ 1.3-urlify.js
@@ -1,23 +1,26 @@
 /*
 URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to hold the additional characters,and that you are given the "true" length of the string. (Note: If implementing in Java,please use a character array so that you can perform this operation in place.)
-
-We can either keep moving everything
 */
 
 function urlify(stringArray) {
   for (let index=0; index<stringArray.length; index++) {
     const char = stringArray[index];
-
+    if (char === ' ') {
+      for (let moveIndex=stringArray.length-1; moveIndex > index; moveIndex--) {
+        stringArray[moveIndex + 2] = stringArray[moveIndex];
+      }
+      stringArray[index] = '%';
+      stringArray[index+1] = '2';
+      stringArray[index+2] = '0';
+    }
   }
 }
 
-let string = "hello";
 
-function addHi(string) {
-  string.concat(' Hi');
-  console.log(string)
-}
-
-addHi(string);
-
-console.log(string);
+// Testing
+const tests = ['hello ', 'hello monsieur', ' what are you saying?'];
+tests.forEach((test) => {
+  const testChars = test.split('');
+  urlify(testChars);
+  console.log(testChars.join(''));
+})
